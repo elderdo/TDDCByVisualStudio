64b0e253ef0acee13304e1eeb5a2936b945f0569
asm-jaime <asm.jaime@gmail.com>
2022-05-05 16:54:18 +1000

    add start structure for simple c tdd

---
+++ CTest/add.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#if __cplusplus 
+	extern "C" int add(int, int);
+#else
+	int add(int, int);
+#endif
---
+++ CTest/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "add.h"
+
+int main(void) {
+	printf("kekezzzzzzz %d", add(3, 5));
+	return 0;
+}
